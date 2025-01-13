f321f1d2ab048e23d07b4c4edca6fd50d4e3bef5
Björn Schembera <bjoern.schembera@mathematik.uni-stuttgart.de>
2025-01-13 15:48:47 +0100

    Added script file for post processing after Widocu generation

---
+++ docs/postProcessWidocu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#####################################################
+# Postprocessing script to perform tasks after Widocu
+# generation to make it automatically feasible for 
+# github pages.
+####################################################
+
+# Generate an index.html
+cp index.html index.html.old
+cp index-en.html index.html
+
+# Insert needed MathJax for all the LaTeX to be parsed
+# It is inserted right behind the HTML <head> tag
+
+# Insert the MathJax script
+sed -i '/<head>/a <script id="MathJax-script" async src="https:\/\/cdn.jsdelivr.net\/npm\/mathjax@3\/es5\/tex-mml-chtml.js"><\/script>' index.html
+# Insert config so that also single character delimiter is recognized
+sed -i '/<head>/a <script>window.MathJax = {   tex: { inlineMath: \[\["\$", "\$\"]\] } }; <\/script>' index.html
+
