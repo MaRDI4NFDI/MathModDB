1cd230d65e68aa63f53296696f70b12015a3d188
Björn Schembera <bjoern.schembera@mathematik.uni-stuttgart.de>
2025-02-04 11:17:16 +0100

    Adapted Scripts and created updated documentation

--- docs/createDocumentation.sh
+++ docs/createDocumentation.sh
@@ -10,7 +10,7 @@
 ####################################################
 
 # Path to the OWL-File of the ontology
-owlOntPath="$HOME/MaRDI/Ontology/model-ontology_main/MaRDIModelOntology_full_noAnon.owl"
+owlOntPath="$1"
 
 # Path to the config file for Widocu
 widocuConfPath="$HOME/MaRDI/Ontology/publicationGithub/MathModDB/docs/config.properties"
@@ -22,16 +22,25 @@ widocuOutputPath=$PWD
 # Path to the Widocu JAR
 widocuJarPath="$HOME/Downloads/widoco-1.4.25-jar-with-dependencies_JDK-17.jar"
 
-#echo -n "Preprocessing Ontology... "
-# Do some preprocessing for better comments
-#./preprocessOntologyScript.sh $owlOntPath
+echo -n "Preprocessing Ontology... "
 
-#echo "DONE"
+# First preprocessing step: Remove all informations objects in the data 
+# properties that have "internal:anonymous-constant". This can be done since
+# they are duplicate entries and Protege and/or OWL can't handle custom 
+# datatype, as our self-defined "LaTeX"
+python3 ./scripts/remove_anonConstants.py $owlOntPath ontology_full_anonRemoved.owl
+
+# Second preprocessing step: Swap rdfs:comment with dcterms:description
+# to have a short description in the heading and the long description in
+# the statement for better readbility
+python3 ./scripts/swap_rdfsTerms.py ontology_full_anonRemoved.owl ontology_full_anonRemoved_swappedTerms.owl
+
+echo -n "DONE"
 
 
 echo -n "Creating WIDOCU... " 
 # Create Widocu 
-java -jar $widocuJarPath -ontFile $owlOntPath -outFolder $widocuOutputPath -confFile $widocuConfPath -getOntologyMetadata -rewriteAll -webVowl -includeAnnotationProperties -uniteSections >logfile.txt 2>&1
+java -jar $widocuJarPath -ontFile ontology_full_anonRemoved_swappedTerms.owl -outFolder $widocuOutputPath -confFile $widocuConfPath -getOntologyMetadata -rewriteAll -webVowl -includeAnnotationProperties -uniteSections >logfile.txt 2>&1
 
 echo "DONE"
 
@@ -39,7 +48,7 @@ echo "DONE"
 echo -n "Postprocessing WIDOCU... "
 # Postprocess the WIDOCU output
 
-./postProcessWidocu.sh
+./scripts/postProcessWidocu.sh
 
 echo "DONE"
 
