44652e1c4ae29f577b9d6039ae921e2ab2d65f21
Aurela Shehu <shehu@wias-berlin.de>
2025-02-27 14:52:22 +0100

    updated doi in references-en.html and updated createDocumentation.sh (mainly replaced python3 with python and some stylistic changes)

--- docs/createDocumentation.sh
+++ docs/createDocumentation.sh
@@ -7,7 +7,7 @@
 #
 # All paths can be set by variables. 
 #
-# call example bash createDocumentation.sh ../MaRDIModelOntology_full.owl widoco-1.4.25-jar-with-dependencies_JDK-17.jar
+# call example ./createDocumentation.sh ../MathModDB_full.owl "C:/Users/shehu/Documents/MaRDI/Widoco/widoco-1.4.25-jar-with-dependencies_JDK-17.jar"
 ####################################################
 
 # Path to the OWL-File of the ontology
@@ -23,34 +23,34 @@ widocuOutputPath=$PWD
 # Path to the Widocu JAR
 widocuJarPath="$2"
 
-echo -n "Preprocessing Ontology... "
+echo "Preprocessing Ontology... "
 
 # First preprocessing step: Remove all informations objects in the data 
 # properties that have "internal:anonymous-constant". This can be done since
 # they are duplicate entries and Protege and/or OWL can't handle custom 
 # datatype, as our self-defined "LaTeX"
-python3 ./scripts/remove_anonConstants.py $owlOntPath ontology_full_anonRemoved.owl
+python ./scripts/remove_anonConstants.py $owlOntPath ontology_full_anonRemoved.owl
 
 # Second preprocessing step: Swap rdfs:comment with dcterms:description
 # to have a short description in the heading and the long description in
 # the statement for better readbility
-python3 ./scripts/swap_rdfsTerms.py ontology_full_anonRemoved.owl ontology_full_anonRemoved_swappedTerms.owl
+python ./scripts/swap_rdfsTerms.py ontology_full_anonRemoved.owl ontology_full_anonRemoved_swappedTerms.owl
 
-echo -n "DONE"
+echo "DONE"
 
 
-echo -n "Creating WIDOCU... " 
+echo "Creating WIDOCO... " 
 # Create Widocu 
 java -jar $widocuJarPath -ontFile ontology_full_anonRemoved_swappedTerms.owl -outFolder $widocuOutputPath -confFile $widocuConfPath -getOntologyMetadata -rewriteAll -htaccess -webVowl -includeAnnotationProperties -uniteSections >logfile.txt 2>&1
 
 echo "DONE"
 
 
-echo -n "Postprocessing WIDOCU... "
-# Postprocess the WIDOCU output
+echo "Postprocessing WIDOCO... "
+# Postprocess the WIDOCO output
 
-bash  scripts/postProcessWidocu.sh
+./scripts/postProcessWidocu.sh
 
-echo -n "DONE"
+echo "DONE"
 
-echo -n "WIDOCU has been created in index.html"
+echo "WIDOCO has been created in index.html"
